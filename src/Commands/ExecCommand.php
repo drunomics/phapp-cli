6b6167dd7d8b8d9c2d11c91be2a6d0d9b0a1aa47
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2016-08-29 12:06:40 +0200

    INF-155: Allow multiple arguments to be passed to phapp exec.

--- src/Commands/ExecCommand.php
+++ src/Commands/ExecCommand.php
@@ -20,27 +20,27 @@ class ExecCommand {
   /**
    * Executes commands based on the environment.
    *
+   * @param string $exec_command The command to execute.
+   * @param string[] $arguments The command arguments.
+   *
    * @command exec
    */
-  public function execCommand($name) {
+  public function execCommand($exec_command, array $arguments) {
     $currentpath = realpath(getcwd());
     if ($strpos = strpos($currentpath, "vcs/web")) {
       $projectpath = substr($currentpath, 0, $strpos);
       $project = basename($projectpath);
       // Detect vagrant environment
       if (file_exists($projectpath . '.vagrant')) {
-        $command = "docker exec " . $project . " " . $name;
+        $exec_command = "docker exec " . $project . " " . $exec_command;
       }
       else {
         // Use the wrapper script for drush.
-        if ($name == "drush") {
-          $command = $projectpath . "/vcs/web/drush.wrapper";
-        }
-        else {
-          $command = $name;
+        if ($exec_command == "drush") {
+          $exec_command = $projectpath . "/vcs/web/drush.wrapper";
         }
       }
-      $this->_exec($command);
+      $this->_exec($exec_command . ' ' . implode(' ', $arguments));
     }
      else {
       print("You need to be in the web directory to execute this command.\n");
