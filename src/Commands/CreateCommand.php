d2c85b587240221a8bb853787ad06f3a943e3d71
Wolfgang Ziegler // fago <fago@wolfgangziegler.net>
2016-09-01 17:29:12 +0200

    Make create arguments optional and ask for missing arguments. Auto-configure the Git repo.

--- src/Commands/CreateCommand.php
+++ src/Commands/CreateCommand.php
@@ -29,9 +29,17 @@ class CreateCommand {
   /**
    * Creates a new app based on a given template.
    *
+   * @param string $name The created app's machine readable name.
+   * @option string $template The template to use.
+   * @option string $template-version A specific version of the template to use. Defaults to using the latest stable version.
+   *
    * @command create
    */
-  public function createApp($name, $template = NULL, $options = ['template' => NULL, 'template-version' => '*']) {
+  public function createApp($name = NULL, $options = ['template' => NULL, 'template-version' => '*']) {
+    if (!$name) {
+      $name = $this->ask("App name (e.g. 'new-site'):");
+    }
+
     if (empty($options['template'])) {
       $question = new ChoiceQuestion('Please select the app template to use:', array_values($this->templates), 1);
 
@@ -45,6 +53,18 @@ class CreateCommand {
     }
 
     $this->_exec("composer create-project $template:{$options['template-version']} --repository='https://packages.drunomics.com' $name");
+
+    $repository_url_pattern = "git@bitbucket.org:drunomics/{{ app_name }}.git";
+    $repository_url = str_replace('{{ app_name }}', $name, $repository_url_pattern);
+    if ($this->confirm("Should the repostiory $repository_url be configured?")) {
+      $this->_exec("cd $name &&
+                git init &&
+                git remote add origin $repository_url &&
+                git add . &&
+                git commit -am 'Initial version.' &&
+                git branch -m develop");
+      $this->say("Git repository has been initialized and pointed to <info>$repository_url</info>. Be sure the repository is set up and execute 'git push' once you are ready.");
+    }
   }
 
 }
