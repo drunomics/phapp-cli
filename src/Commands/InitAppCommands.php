9635483527a61ef0d094f7327924e8a043e28a04
fago <nuppla@zites.net>
2017-11-21 02:12:56 +0100

    Add the init command.

---
+++ src/Commands/InitAppCommands.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace drunomics\Phapp\Commands;
+
+use drunomics\Phapp\PhappCommandBase;
+use drunomics\Phapp\ServiceUtil\BuildCommandsTrait;
+
+/**
+ * Inits the app.
+ */
+class InitAppCommands extends PhappCommandBase  {
+
+  use BuildCommandsTrait;
+
+  /**
+   * Initializes the app.
+   *
+   * Initializes the app as defined by the app; i.e., either per installing from
+   * scratch or importing a database dump.
+   *
+   * @option bool $build Build before running an update if the app is in
+   *   development mode.
+   *
+   * @command init
+   */
+  public function initApp(array $options = ['build' => TRUE]) {
+    $collection = $this->collectionBuilder();
+    $collection->setProgressIndicator(NULL);
+    if (getenv('PHAPP_ENV_MODE') == 'development' && $options['build']) {
+      $collection->addCode(function() {
+        $this->io()->title('Building...');
+      });
+      $collection->addTask(
+        $this->getBuildCommands()->build(['clean' => FALSE])
+      );
+      $collection->addCode(function() {
+        $this->io()->title('Initializing...');
+      });
+    }
+    $command = $this->phappManifest->getCommand('init');
+    $collection->addTask(
+      $this->taskExec($command)
+    );
+    return $collection;
+  }
+}
